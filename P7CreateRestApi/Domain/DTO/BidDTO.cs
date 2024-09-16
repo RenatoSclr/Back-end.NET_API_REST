2be10fb5a9e93d8e85a1ca56878504f6080f6525
Renato Sicler <renatosicler33@gmail.com>
2024-09-16 12:10:10 +0200

    Create BidDTO

---
+++ P7CreateRestApi/Domain/DTO/BidDTO.cs
@@ -0,0 +1,10 @@
+﻿namespace P7CreateRestApi.Domain.DTO
+{
+    public class BidDTO
+    {
+        public int BidDTOId { get; set; }
+        public string Account { get; set; }
+        public string BidDTOType { get; set; }
+        public double? BidDTOQuantity { get; set; }
+    }
+}
