0a1b6a090879e8a11525a7c8afd7a50afc22b884
Renato Sicler <renatosicler33@gmail.com>
2024-09-18 12:19:15 +0200

    Create CurvePointDTO

---
+++ P7CreateRestApi/Domain/DTO/CurvePointDTO.cs
@@ -0,0 +1,12 @@
+﻿namespace P7CreateRestApi.Domain.DTO
+{
+    public class CurvePointDTO
+    {
+        public int Id { get; set; }
+        public byte? CurveDTOId { get; set; }
+        public DateTime? AsOfDate { get; set; }
+        public double? Term { get; set; }
+        public double? CurvePointDTOValue { get; set; }
+        public DateTime? CreationDate { get; set; }
+    }
+}
