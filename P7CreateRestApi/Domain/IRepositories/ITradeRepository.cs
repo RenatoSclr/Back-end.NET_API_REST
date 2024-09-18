72b510efdb2cd8c872ea21e6429ed41339f2e6d7
Renato Sicler <renatosicler33@gmail.com>
2024-09-18 14:26:38 +0200

    Create TradeRepository

---
+++ P7CreateRestApi/Domain/IRepositories/ITradeRepository.cs
@@ -0,0 +1,8 @@
+﻿namespace Dot.Net.WebApi.Domain.IRepositories
+{
+    public interface ITradeRepository : IRepository<Trade>
+    {
+        Task UpdateAsync(Trade entity);
+        Task SaveAsync();
+    }
+}
---
+++ P7CreateRestApi/Repositories/TradeRepository.cs
@@ -0,0 +1,29 @@
+﻿using Dot.Net.WebApi.Data;
+using Dot.Net.WebApi.Domain;
+using Dot.Net.WebApi.Domain.IRepositories;
+using P7CreateRestApi.Repositories;
+
+namespace Dot.Net.WebApi.Repositories
+{
+    public class TradeRepository : Repository<Trade>, ITradeRepository
+    {
+        private readonly LocalDbContext _context;
+        public TradeRepository(LocalDbContext context) : base(context)
+        {
+            _context = context;
+        }
+
+       
+        public async Task SaveAsync()
+        {
+            await _context.SaveChangesAsync();
+        }
+
+        
+        public async Task UpdateAsync(Trade entity)
+        {
+            _context.Update(entity);
+            await SaveAsync(); 
+        }
+    }
+}
